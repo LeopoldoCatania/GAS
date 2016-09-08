3781d0b9d56194c834b1de7541e04da895be4f90
Leopoldo Catania <leopoldo.catania@uniroma2.it>
2016-09-08 11:01:21 +0200

    version 0.1.4

--- src/mvnorm.h
+++ src/mvnorm.h
@@ -11,7 +11,6 @@ double dmvnorm_ThetaParam(arma::vec vY,
                           arma::vec vTheta,
                           int iN,
                           bool bLog = false);
-arma::mat rmvnorm_mat(int iN, arma::vec vMu, arma::mat mSigma);
 arma::mat rmvnorm_ThetaParam(arma::vec vTheta,int iN, int iJ) ;
 arma::vec mMVNORM_mean(arma::vec vTheta, int iN);
 arma::mat mMVNORM_cov(arma::vec vTheta, int iN);
