f3116ed87e0a5ddf5157cc216e867e1f92c1e6e8
LeopoldoCatania <leopoldo.catania@uniroma2.it>
2017-06-06 17:03:57 +0200

    default solnp values

--- R/OptimiserWrapper.R
+++ R/OptimiserWrapper.R
@@ -2,7 +2,8 @@ fn.solnp <- function(par0, data, GASSpec, FUN) {
 
   solver.ctr <- list(trace = 0, rho = 1, outer.iter = 400, inner.iter = 800, delta = 1e-07, tol = 1e-08)
 
-  optimiser = suppressWarnings(solnp(par0, FUN, data = data, GASSpec = GASSpec, control = solver.ctr))
+  # optimiser = suppressWarnings(solnp(par0, FUN, data = data, GASSpec = GASSpec, control = solver.ctr))
+  optimiser = suppressWarnings(solnp(par0, FUN, data = data, GASSpec = GASSpec))
 
   out = list(pars = optimiser$pars,
              value = tail(optimiser$values, 1),
