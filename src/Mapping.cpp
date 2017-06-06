0c14c5d0835dd525fe8fe6eab61064a0ee4cd557
LeopoldoCatania <leopoldo.catania@uniroma2.it>
2017-06-06 14:29:35 +0200

    rob score

--- src/Mapping.cpp
+++ src/Mapping.cpp
@@ -10,8 +10,8 @@ const double dUpperShape = 50.0;
 const double dLowerSkewFS = 0.50;
 const double dUpperSkewFS = 1.50;
 
-const double dNumericalUpperLimit = 1e5;
-const double dNumericalLowerLimit = -1e7;
+const double dNumericalUpperLimit = 1e10;
+const double dNumericalLowerLimit = -1e10;
 const double dNumericalUnderflow  = 1e-10;
 
 double Map(double dX, double dL,double dU) {
